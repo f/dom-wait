5d25a5767ef2629d12f7a15a526e828784b58c2f
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2018-07-23 17:35:52 +0300

    nested waiters

--- src/js/dom-wait.js
+++ src/js/dom-wait.js
@@ -1,9 +1,8 @@
 (function (global) {
   function $ready(f, d, e) {
-    _f = function () { f(); }
     d = document
     e = 'addEventListener'
-    d[e] ? d[e]('DOMContentLoaded', _f) : window.attachEvent('onload', _f)
+    d[e] ? d[e]('DOMContentLoaded', f) : window.attachEvent('onload', f)
   }
 
   function __waitFor(waiter, fn) {
@@ -24,7 +23,7 @@
       var nodes = this.childNodes;
       for (var i = 0; i < nodes.length; i++) {
         if (nodes[i].classList && nodes[i].classList.contains('waiting')) {
-          this.__waiterPlaceholder = document.createComment('waiter for ' + this.getAttribute('data-wait-for'));
+          this.__waiterPlaceholder = document.createComment('WAITER: ' + this.getAttribute('data-wait-for'));
           this.__waiterPlaceholder.__isWaiter = true;
           this.__waiter = nodes[i];
           this.replaceChild(this.__waiterPlaceholder, this.__waiter);
@@ -38,7 +37,14 @@
       var nodes = this.childNodes;
       for (var i = 0; i < this.childNodes.length; i++) {
         if (nodes[i] === this.__waiterPlaceholder) continue;
-        this.__contentPlaceholder = document.createComment('(detached) ' + (nodes[i].tagName || ''));
+        var content;
+        if (nodes[i].nodeType === 1 /*ELEMENT_NODE*/) {
+          content = '<' + nodes[i].tagName + '... />';
+        } else {
+          content = nodes[i].textContent.trim();
+        }
+
+        this.__contentPlaceholder = document.createComment(content ? 'LOADING: ' + content.toLowerCase() : '');
         this.__contentPlaceholder.__originalContent = nodes[i];
         this.replaceChild(this.__contentPlaceholder, nodes[i]);
       }
@@ -71,10 +77,15 @@
       this.waitingFor.splice(this.waitingFor.indexOf(waiter), 1);
       $end(waiter);
     },
+    ready: function (fn) {
+      $ready(fn);
+    },
     is: function (waiter) {
       return this.waitingFor.indexOf(waiter) > -1;
     }
   };
 
-  $ready($init);
+  $ready(function () {
+    $init();
+  });
 })(self);
